4de8e2487428941d34d9a273085fff6507a99ad4
hyzx8 <hyzx8@DESKTOP-5EJBECV>
2019-06-24 23:31:30 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace HostsFileSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileContent = new System.Windows.Forms.TextBox();
+            this.lbEnabled = new System.Windows.Forms.ListBox();
+            this.lbDisabled = new System.Windows.Forms.ListBox();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // txtFileContent
+            // 
+            this.txtFileContent.Location = new System.Drawing.Point(52, 31);
+            this.txtFileContent.Multiline = true;
+            this.txtFileContent.Name = "txtFileContent";
+            this.txtFileContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFileContent.Size = new System.Drawing.Size(668, 84);
+            this.txtFileContent.TabIndex = 0;
+            // 
+            // lbEnabled
+            // 
+            this.lbEnabled.FormattingEnabled = true;
+            this.lbEnabled.ItemHeight = 12;
+            this.lbEnabled.Location = new System.Drawing.Point(12, 192);
+            this.lbEnabled.Name = "lbEnabled";
+            this.lbEnabled.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbEnabled.Size = new System.Drawing.Size(466, 244);
+            this.lbEnabled.TabIndex = 1;
+            this.lbEnabled.DoubleClick += new System.EventHandler(this.Lb_DoubleClick);
+            // 
+            // lbDisabled
+            // 
+            this.lbDisabled.FormattingEnabled = true;
+            this.lbDisabled.ItemHeight = 12;
+            this.lbDisabled.Location = new System.Drawing.Point(581, 192);
+            this.lbDisabled.Name = "lbDisabled";
+            this.lbDisabled.Size = new System.Drawing.Size(457, 244);
+            this.lbDisabled.TabIndex = 2;
+            this.lbDisabled.DoubleClick += new System.EventHandler(this.Lb_DoubleClick);
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Location = new System.Drawing.Point(484, 232);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveAll.TabIndex = 3;
+            this.btnRemoveAll.Text = ">>";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.Move_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Move_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(484, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Move_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(484, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Move_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "已启用";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "已禁用";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(750, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(52, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(425, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "建议删除无用的注释，否则在启用全部的时候会把注释和说明内容的注释取消掉";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(945, 13);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(71, 12);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "更多内容...";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.lbDisabled);
+            this.Controls.Add(this.lbEnabled);
+            this.Controls.Add(this.txtFileContent);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hosts 文件 切换工具 ，足够简单无需说明";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileContent;
+        private System.Windows.Forms.ListBox lbEnabled;
+        private System.Windows.Forms.ListBox lbDisabled;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HostsFileSwitch
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            if (args.Length > 0)
+            {
+               
+                AppLoad();
+                if (args[0].ToLower() == "e")
+                {
+                    ChangeState("<<");
+                }
+                else if (args[0].ToLower() == "d")
+                {
+                    ChangeState(">>");
+                }
+
+                Application.Exit(); 
+            }
+        
+        }
+
+        const string HOSTSFILEPATH = @"C:\Windows\System32\drivers\etc\hosts";
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AppLoad();
+
+        }
+
+        private void AppLoad()
+        {
+            string fileContent = File.ReadAllText(HOSTSFILEPATH);
+            txtFileContent.Text = fileContent;
+
+            var contentList = fileContent.Split(new string[] { "\r\n" },
+                StringSplitOptions.RemoveEmptyEntries).Reverse();
+            //排除掉以# 号开头的数据
+
+            var enabledList = contentList.Where(o => !o.StartsWith("#"));
+            //排除掉另外一部分数据
+            var disabledList = contentList.Except(enabledList);
+            lbEnabled.Items.Clear();
+            lbDisabled.Items.Clear();
+            lbEnabled.Items.AddRange(enabledList.ToArray());
+            lbDisabled.Items.AddRange(disabledList.ToArray());
+        }
+
+        private void Save(string[] allList)
+        {
+            try
+            {
+                if (File.Exists(HOSTSFILEPATH))
+                {
+                    File.Delete(HOSTSFILEPATH);
+                }
+
+                File.AppendAllLines(HOSTSFILEPATH, allList.ToArray());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void Move_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            var oprName = btn.Text;
+
+            ChangeState(oprName);
+        }
+
+        private void ChangeState(string oprName)
+        {
+            switch (oprName)
+            {
+                //禁用所有
+                case ">>":
+                    var swapList = lbEnabled.Items.Cast<string>();
+                    var targetList = swapList.Select(o => "#" + o);
+                    lbDisabled.Items.AddRange(targetList.ToArray());
+                    lbEnabled.Items.Clear();
+
+                    break;
+                //禁用选中
+                case ">":
+                    swapList = lbEnabled.SelectedItems.Cast<string>();
+                    targetList = swapList.Select(o => "#" + o);
+                    lbDisabled.Items.AddRange(targetList.ToArray());
+                    for (int i = 0; i < lbEnabled.SelectedItems.Count; i++)
+                    {
+                        lbEnabled.Items.Remove(lbEnabled.SelectedItems[i]);
+                    }
+                    break;
+
+                //启用全部
+                case "<<":
+                    swapList = lbDisabled.Items.Cast<string>();
+                    targetList = swapList.Select(o => o.TrimStart(" #".ToCharArray()));
+                    lbEnabled.Items.AddRange(targetList.ToArray());
+                    lbDisabled.Items.Clear();
+
+                    break;
+                //启用选中
+                case "<":
+                    swapList = lbDisabled.SelectedItems.Cast<string>();
+                    targetList = swapList.Select(o => o.TrimStart(" #".ToCharArray()));
+                    lbEnabled.Items.AddRange(targetList.ToArray());
+                    lbDisabled.Items.Remove(lbDisabled.SelectedItems);
+                    for (int i = 0; i < lbDisabled.SelectedItems.Count; i++)
+                    {
+                        lbDisabled.Items.Remove(lbDisabled.SelectedItems[i]);
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+            var lsEnabled = lbEnabled.Items.Cast<string>();
+            var lsDisabled = lbDisabled.Items.Cast<string>();
+
+            var allList = new List<string>();
+
+
+            allList.AddRange(lsEnabled);
+            allList.AddRange(lsDisabled);
+            allList.Reverse();
+
+            Save(allList.ToArray());
+            AppLoad();
+        }
+
+        private void Lb_DoubleClick(object sender, EventArgs e)
+        {
+            if (sender is ListBox)
+            {
+                var lb = (ListBox)sender;
+                if (lb.Name == "lbEnabled")
+                {
+                    ChangeState(">");
+                }
+                else
+                {
+                    ChangeState("<");
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Save(txtFileContent.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+            AppLoad();
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.cnblogs.com/Qbit/p/11080198.html");
+        }
+    }
+}
